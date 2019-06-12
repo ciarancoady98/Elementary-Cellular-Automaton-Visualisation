84cc1971660871e208fb6c5b5c36d113a47f2489
Ciaran Coady <46931188+ciarancoady98@users.noreply.github.com>
2019-06-12 18:39:26 +0100

    Initialiation of pygame
    Established initial program structure with functions to exit the visualisation if the escape key is pressed or limit is reached on the generation count

--- ElementaryCellularAutomatonSim.py
+++ ElementaryCellularAutomatonSim.py
@@ -1,5 +1,40 @@
-import pygame, random, sys
+import pygame
 from pygame.locals import *
 
+
+#Definition of constants
 WINDOWWIDTH = 600
 WINDOWHEIGHT = 600
+TEXTCOLOR = (255, 255, 255)
+BACKGROUNDCOLOUR = (0, 0, 0)
+GENERATIONCOUNT = 20
+
+#Exits when the simulation is complete
+def end():
+    pygame.quit()
+
+#Checks are any of the exit conditions satisified
+def checkExitConditions(currentGeneration):
+    #Pressing escape quits
+    for event in pygame.event.get():
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE: 
+                    end()
+    #Reaching the GENERATIONCOUNT quits
+    if currentGeneration == GENERATIONCOUNT:
+        end()
+
+
+
+
+#Initialisation of the visualisation window
+pygame.init();
+window = pygame.display.set_mode((WINDOWHEIGHT, WINDOWWIDTH))
+
+
+#Main display loop
+while True:
+    window.fill(BACKGROUNDCOLOUR)
+    checkExitConditions(1)
+    
+    
